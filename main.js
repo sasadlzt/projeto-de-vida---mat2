bce77b6dbd5f3520fcc1ec130511de4b3831f24e
sasadlzt <sarah.dalzoto@escola.pr.gov.br>
2026-04-17 09:43:57 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
 console.log(botoes);
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function (){
+      for(let j=0;j<botoes.length;j++){
+        botoes[j].classicList.remove("ativo");
+      } 
+      botoes[i].classicList.add("ativo"); 
+    }
+    console.log();
+}
